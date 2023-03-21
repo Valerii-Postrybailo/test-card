1daf83a36e231284951fef4e7be1c1179085ac56
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2023-03-21 20:21:10 +0200

    added local storage

--- js/follow.js
+++ js/follow.js
@@ -1,6 +1,11 @@
 const btnEl = document.querySelector(".card__btn");
 const followersNumberEl = document.querySelector(".card__user-followers-span");
 
+btnEl.textContent = localStorage.getItem("activeBtnText") || btnEl.textContent;
+btnEl.style.backgroundColor = localStorage.getItem("activeBtnColor") || btnEl.style.backgroundColor;
+followersNumberEl.textContent = localStorage.getItem("followersNumber") || followersNumberEl.textContent;
+
+
 const deleteComa = (content) => Number(content.replace(/[\s.,%]/g, ''));
 
 const addComa = (content) => {
@@ -22,22 +27,19 @@ const addComa = (content) => {
 const handleClick = () => {
 
   if (btnEl.textContent === "Follow") {
-    followersNumberEl.textContent = deleteComa(followersNumberEl.textContent) + 1
-    followersNumberEl.textContent = addComa(followersNumberEl.textContent)
-
-    return (
-      btnEl.textContent = "Following",
-      btnEl.style.backgroundColor = "#5CD3A8"
-      )
+    followersNumberEl.textContent = deleteComa(followersNumberEl.textContent) + 1;
+    followersNumberEl.textContent = addComa(followersNumberEl.textContent);
+    btnEl.textContent = "Following";
+    btnEl.style.backgroundColor = "#5CD3A8";
   } else {
     followersNumberEl.textContent = deleteComa(followersNumberEl.textContent) - 1
     followersNumberEl.textContent = addComa(followersNumberEl.textContent)
-
-    return (
-      btnEl.textContent = "Follow",
-      btnEl.style.backgroundColor = "#EBD8FF"
-    )
+    btnEl.textContent = "Follow",
+    btnEl.style.backgroundColor = "#EBD8FF"
   }
+  localStorage.setItem("followersNumber", followersNumberEl.textContent );
+  localStorage.setItem("activeBtnText", btnEl.textContent);
+  localStorage.setItem("activeBtnColor", btnEl.style.backgroundColor);
 }
 
 followersNumberEl.textContent = addComa(followersNumberEl.textContent)
